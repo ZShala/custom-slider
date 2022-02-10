8d534d1a28b4a3cd3520dc794db930964bf134a7
Zana Shala <zanashala2015@gmail.com>
2022-02-10 21:58:31 +0100

    Changing Slider Style of Swipe

--- src/pages/Home/components/Swiper/index.jsx
+++ src/pages/Home/components/Swiper/index.jsx
@@ -8,11 +8,11 @@ const ImageSwiper = ({ slides }) => {
   const length = slides.length;
 
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+    setCurrent((prevState) => (prevState === length - 1 ? 0 : prevState + 1));
   };
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
+    setCurrent((prevState) => (prevState === 0 ? length - 1 : prevState - 1));
   };
 
   if (!Array.isArray(slides) || slides.length <= 0) {
@@ -20,20 +20,31 @@ const ImageSwiper = ({ slides }) => {
   }
 
   return (
-    <div className="slider">
-      <MdOutlineArrowBackIos className="left-arrow" onClick={prevSlide} />
+    <div className="slider-container">
+      <MdOutlineArrowBackIos
+        className="left-arrow arrow left"
+        onClick={prevSlide}
+      />
 
-      <MdOutlineArrowForwardIos className="right-arrow" onClick={nextSlide} />
+      <MdOutlineArrowForwardIos
+        className="right-arrow arrow right"
+        onClick={nextSlide}
+      />
+      <div className="slider">
 
-      {slides?.map((slide, index) => {
-        return (
-          <div className={`slide ${index === current && "active"}`} key={index}>
-            {index === current && (
+        {slides?.map((slide, index) => {
+          return (
+            <div
+              className={`slide ${index === current && "active"} ${
+                index < current ? "prev" : index > current ? "next" : ""
+              }`}
+              key={index}
+            >
               <img src={slide.url} alt={slide.title} className="image" />
-            )}
-          </div>
-        );
-      })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
